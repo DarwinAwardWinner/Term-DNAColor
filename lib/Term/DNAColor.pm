6e64ad34295c200c4377bd3834bf660058a94c13
Ryan C. Thompson <rct@thompsonclan.org>
2011-01-20 19:47:31 -0800

    First working version

--- lib/Term/DNAColor.pm
+++ lib/Term/DNAColor.pm
@@ -3,9 +3,61 @@ use warnings;
 use utf8;
 
 package Term::DNAColor;
-# ABSTRACT: [1-line description of module]
+# ABSTRACT: Add colors to DNA and RNA sequences
 
-# [MODULE IMPLEMENTATION GOES HERE]
+use Exporter::Simple;
+use Term::ANSIColor qw(color);
+
+sub _get_nucl_color {
+    my $nucl_colors = {
+        A => 'green',
+        T => 'red',
+        U => 'red',             # Support RNA too!
+        C => 'blue',
+        G => 'yellow',
+    };
+    return color($nucl_colors->{$_[0]} || 'reset') . color('bold');
+}
+
+sub _wrap_nucl_in_color {
+    return _get_nucl_color($_[0]) . $_[0] . color('reset');
+}
+
+=function colordna
+
+Takes a string representing a DNA sequence and adds ANSI color codes
+to the following nucleotides:
+
+=for :list
+* A: green
+* T: red
+* C: blue
+* G: yellow
+
+U is colorized like T, to accomodate RNA sequences.
+
+In addition, the entire sequence is rendered in bold.
+
+This function is exported by default.
+
+=cut
+
+sub colordna : Exported {
+    my @seq_chars = split //, shift;
+    my @colored_seq_chars = map { _wrap_nucl_in_color($_) } @seq_chars;
+    return join "", @colored_seq_chars;
+}
+
+=function colorrna
+
+This is simply an alias for C<colordna>. Unlike C<colordna>, it is not
+exported by default, but only by request.
+
+=cut
+
+sub colorrna : Exportable {
+    return colordna(@_);
+}
 
 1; # Magic true value required at end of module
 __END__
@@ -14,15 +66,18 @@ __END__
 
     use Term::DNAColor;
 
-    [Sample usage of module]
+    print colordna("ATCGGTCNNNTAGCTGAN"), "\n";
 
 =head1 DESCRIPTION
 
-[EXPLAIN]
+This module mainly provides a function, C<colordna>, that takes a DNA
+sequence and wraps unambiguous nucleotides in ANSI color codes, so
+that you can print the sequence to a terminal and have it come out
+colored.
 
 =head1 BUGS AND LIMITATIONS
 
-[KNOWN BUGS]
+Colors are not configurable.
 
 Please report any bugs or feature requests to
 C<rct+perlbug@thompsonclan.org>.
@@ -30,7 +85,7 @@ C<rct+perlbug@thompsonclan.org>.
 =head1 SEE ALSO
 
 =for :list
-[* L<Related::Module> - How it is related]
+* L<Term::ANSIColor> - Provides the ANSI color codes for this module
 
 =head1 DISCLAIMER OF WARRANTY
 
---
+++ t/colordna.t
@@ -0,0 +1,20 @@
+#!perl
+use Test::More;
+use Term::ANSIColor qw(colorstrip color);
+use Term::DNAColor;
+
+my $seq = "ATGCNATGCN";
+
+my $colored_seq = colordna($seq);
+ok($colored_seq, "colordna returned something");
+is(colorstrip($colored_seq), $seq, "colordna does not change characters, only adds colors between them");
+
+my $reset_end_regexp = quotemeta(color('reset')) . '$';
+
+ok($colored_seq =~ m{$reset_end_regexp}, "colordna ends with a reset");
+
+TODO: {
+    local $TODO = "I don't know how to test for ANSI color code equivalence.";
+}
+
+done_testing();
